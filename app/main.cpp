b844bfa2e279dd97811d7642203c4b02c38ac4a6
Fernando Caletti <fe.caletti@gmail.com>
2022-11-12 10:25:51 -0300

    feat: added buttons and list box

--- app/main.cpp
+++ app/main.cpp
@@ -3,8 +3,10 @@
 #include <wx/wxprec.h>
 #include <wx/wx.h>
 #include <wx/frame.h>
+#include <wx/listbox.h>
 
 #include <stdio.h>
+#include <vector.h>
 
 using namespace std;
 
@@ -27,8 +29,13 @@ class MyFrame : public wxFrame
         void OnHello(wxCommandEvent& event);
         void OnExit(wxCommandEvent& event);
         void OnAbout(wxCommandEvent& event);
-        wxTextCtrl *firstText;
-        wxButton *btn;
+        
+        wxTextCtrl *logTxtCtrl;
+        wxListBox *routeListBox;
+        wxButton *addBtn;
+        wxButton *removeBtn;
+        wxButton *updateBtn;
+        wxButton *openInBrowserBtn;
 };
 
 enum
@@ -46,12 +53,12 @@ bool MyApp::OnInit()
 }
 
 BEGIN_EVENT_TABLE ( MyFrame, wxFrame )
-    EVT_BUTTON ( 153, MyFrame::TestMethod ) // Tell the OS to run MainFrame::OnExit when
-  END_EVENT_TABLE() // The button is pressed
+    EVT_BUTTON ( 189, MyFrame::TestMethod ) // Tell the OS to run test method onclick btn 189
+END_EVENT_TABLE() // The button is pressed
 
 
 MyFrame::MyFrame()
-    : wxFrame(nullptr, wxID_ANY, "Hello World ---- T")
+    : wxFrame(nullptr, wxID_ANY, "Hello World ---- T", wxDefaultPosition, wxSize(500, 500))
 {
     wxMenu *menuFile = new wxMenu();
     menuFile->Append(ID_Hello, "&Hello...\tCtrl-H", "Help string shown in status bar for this menu item");
@@ -74,19 +81,30 @@ MyFrame::MyFrame()
     Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
 
-    firstText = new wxTextCtrl(this, 152,
-      wxT("Hi!"), wxDefaultPosition, wxDefaultSize,
-      wxTE_MULTILINE | wxTE_RICH , wxDefaultValidator, wxTextCtrlNameStr);
+
+
+    routeListBox = new wxListBox(this, 154, wxPoint(250, 10), wxSize(240, 200));
+
+        logTxtCtrl = new wxTextCtrl(this, 152,
+      wxT("Hi!"), wxPoint(250, 220), wxSize(240, 200),
+      wxTE_MULTILINE | wxTE_RICH | wxTE_READONLY, wxDefaultValidator, wxTextCtrlNameStr);
     
-    btn = new wxButton(this, 153, wxT("TBTN"), wxPoint(100, 100), wxDefaultSize, 0);
-    wxRadioButton *rbtn = new wxRadioButton(this, 154, wxT("RADIO"), wxPoint(100, 130));
+    addBtn = new wxButton(this, 189, wxT("ADD"), wxPoint(30, 40), wxDefaultSize, 0);
+    updateBtn = new wxButton(this, 190, wxT("UPDATE"), wxPoint(30, 70), wxDefaultSize, 0);
+    removeBtn = new wxButton(this, 191, wxT("REMOVE"), wxPoint(30, 100), wxDefaultSize, 0);
+    openInBrowserBtn = new wxButton(this, 192, wxT("BROWSE"), wxPoint(30, 130), wxDefaultSize, 0);
     //WE CAN CREATE A CONTAINER LIKE wxBoxSizer and add radio buttons with style=wxRB_GROUP to it
+
+    // Layout();
+    // wxSize size = GetSize();
+    // this->SetMinSize(wxSize(500, 500));
+    // Layout();
 }
 
 void MyFrame :: TestMethod(wxCommandEvent& event)
 {
-    cout << "TESTOU!" << endl;
-    this->firstText->AppendText("\nteste");
+//     cout << "TESTOU!" << endl;
+    this->logTxtCtrl->AppendText("\nteste");
 }
 
 void MyFrame::OnExit(wxCommandEvent& event)
